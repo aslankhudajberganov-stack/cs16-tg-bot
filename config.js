bb5eee7e84ce1582f95f65be7f475de8455b951f
aslankhudajberganov-stack <aslan.khudajberganov@gmail.com>
2026-01-02 00:34:24 +0500

    Clear serverList in config.js
    Removed server entries from serverList in config.js.

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 module.exports = {
   token: process.env.BOT_TOKEN,
-  serverList: [
-    { host: '46.174.55.32', port: 27015 },
-    { host: '62.122.213.153', port: 27015 }
-  ]
+  serverList: []
 };
