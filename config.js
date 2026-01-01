4127e67c08d8ac57eab876a4a17581198ad0f78b
aslankhudajberganov-stack <aslan.khudajberganov@gmail.com>
2026-01-01 18:12:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-token: '8511940561:AAGaTNPestkt4sM_Ig8fuKazwgX64AVVjNw',
+  token: '8511940561:AAGaTNPestkt4sM_Ig8fuKazwgX64AVVjNw',
   server: {
-    host: "46.174.55.32", // IP сервера
-    port: 27015           // порт сервера
+    host: '46.174.55.32',
+    port: 27015
   }
 };
