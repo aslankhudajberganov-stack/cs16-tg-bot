fc9d1892179f1765bd0ad803b889adb96f31ed36
aslankhudajberganov-stack <aslan.khudajberganov@gmail.com>
2026-01-02 17:50:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  admins: [6387957935], // ваш ID админа
   servers: [
     { host: '46.174.55.32', port: 27015, name: 'SPIRIT [CLASSIC]' },
     { host: '62.122.213.153', port: 27015, name: 'SD' },
     { host: '37.230.228.21', port: 27015, name: '© КВАРТАЛ 32' }
+  ],
+  admins: [
+    6387957935 // @leva_sdd
   ]
 };
