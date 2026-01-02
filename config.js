9a6e21a2d1536fdd400d68efebab13bce370aaad
aslankhudajberganov-stack <aslan.khudajberganov@gmail.com>
2026-01-02 15:37:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 module.exports = {
   token: process.env.BOT_TOKEN,
-  admins: [123456789], // потом пригодится
-  servers: []
+
+  // укажи СВОЙ telegram ID
+  admins: [123456789],
+
+  servers: [
+    { host: '46.174.55.32', port: 27015 },
+    { host: '62.122.213.153', port: 27015 }
+  ]
 };
