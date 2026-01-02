8391b7ea96cee3fc53c6d83b51901c52eb6a9a6c
aslankhudajberganov-stack <aslan.khudajberganov@gmail.com>
2026-01-02 20:16:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  token: process.env.BOT_TOKEN, // токен бота из переменной окружения
-  admins: [6387957935],         // ID админа
+  token: process.env.BOT_TOKEN,
 
-  // Три сервера по умолчанию
+  admins: [6387957935],
+
   servers: [
-    { host: '46.174.55.32', port: 27015, name: 'SPIRIT [CLASSIC]' },
-    { host: '62.122.213.153', port: 27015, name: 'SD' },
-    { host: '37.230.228.21', port: 27015, name: '© КВАРТАЛ 32' }
+    {
+      name: 'SPIRIT [CLASSIC]',
+      host: '46.174.55.32',
+      port: 27015
+    },
+    {
+      name: 'SD',
+      host: '62.122.213.153',
+      port: 27015
+    },
+    {
+      name: '© КВАРТАЛ 32',
+      host: '37.230.228.21',
+      port: 27015
+    }
   ]
 };
