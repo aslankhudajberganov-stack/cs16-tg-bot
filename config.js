a87d3a286acfd8ba2412fdb426209c2d978a243e
aslankhudajberganov-stack <aslan.khudajberganov@gmail.com>
2026-01-02 19:25:40 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  admins: [6387957935], // ID администратора
+  token: process.env.BOT_TOKEN,
+
+  // укажи СВОЙ telegram ID
+  admins: [123456789],
+
   servers: [
-    { host: '46.174.55.32', port: 27015, name: 'SPIRIT [CLASSIC]' },
-    { host: '62.122.213.153', port: 27015, name: 'SD' },
-    { host: '37.230.228.21', port: 27015, name: '© КВАРТАЛ 32' }
+    { host: '46.174.55.32', port: 27015 },
+    { host: '62.122.213.153', port: 27015 }
   ]
 };
